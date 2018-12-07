48744302dac8e6f8f84f7c35aa59a5c369f85bc7
Uttam Panchasara <panchasarauttam@gmail.com>
2018-12-07 18:20:57 +0530

    modified directory reference

--- api/v1/users.js
+++ api/v1/users.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express();
-var db = require('F://Node/sample/db');
-var servedata = require('F://Node/sample/api/servedata');
+var db = require('../../db');
+var servedata = require('../servedata');
 var bodyParser = require('body-parser');
 var apiversion = require('./apiversion');
 
@@ -12,22 +12,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 var endPoint = apiversion.url();
 
 // get all users
-app.get(endPoint +'/users', function(req, res) {
-		
+app.get(endPoint + '/users', function (req, res) {
+
 	var userId = req.query.id;
 
-	if(!userId){
+	if (!userId) {
 		var sql = "SELECT * FROM users";
-		db.query(sql,function(err, result, fields){
+		db.query(sql, function (err, result, fields) {
 			if (err) throw err;
-			
+
 			res.status(200).send(
 				servedata.success(result)
 			);
 		});
-	}else {
-		var sql = "SELECT * FROM users WHERE id = '" + userId +"' LIMIT 1";
-		db.query(sql,function(err, result, fields){
+	} else {
+		var sql = "SELECT * FROM users WHERE id = '" + userId + "' LIMIT 1";
+		db.query(sql, function (err, result, fields) {
 			if (err) throw err;
 			res.status(200).send(
 				servedata.success(result)
@@ -36,21 +36,21 @@ app.get(endPoint +'/users', function(req, res) {
 	}
 });
 
-app.post(endPoint +'/users', function(req, res) {
-	
+app.post(endPoint + '/users', function (req, res) {
+
 	console.log(req.body.name);
-	
-	if(req.body){		
+
+	if (req.body) {
 		var name = req.body.name;
 		var email = req.body.email;
 		var number = req.body.number;
 
-		if(name && number && email) {
+		if (name && number && email) {
 			var sql = "INSERT INTO users(u_name,u_email,u_number) VALUES('" + name + "', '" + email + "', '" + number + "')";
-			db.query(sql,function(err, result, fields){
+			db.query(sql, function (err, result, fields) {
 				if (err) throw err;
-				
-				db.query("SELECT * FROM users WHERE u_email = '" + email + "' LIMIT 1", function(err,result,fields){
+
+				db.query("SELECT * FROM users WHERE u_email = '" + email + "' LIMIT 1", function (err, result, fields) {
 					res.status(200).send(
 						servedata.success(result)
 					);
@@ -61,9 +61,9 @@ app.post(endPoint +'/users', function(req, res) {
 				servedata.error('data missing!')
 			);
 		}
-	}else{
+	} else {
 		return res.status(400).send(
-				servedata.error('empty body')
+			servedata.error('empty body')
 		);
 	}
 });
