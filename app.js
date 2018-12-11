25fd338a1b8325afdb4b4d6ab32d15fe1b588b20
Uttam Panchasara <uttam.panchasara@multidots.com>
2018-12-11 14:46:24 +0530

    updated api endPoint

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 var recordsRouter = require('./routes/records');
 
 var app = express();
+var endPoint = '/api/v1/';
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,8 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/records', recordsRouter);
+app.use(endPoint + 'users', usersRouter);
+app.use(endPoint + 'records', recordsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
