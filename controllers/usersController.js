a2d1c462babb990b8332a367c5609752d80c926f
UttamPanchasara <panchasarauttam@gmail.com>
2018-12-12 10:43:29 +0530

    modified socket implementation

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,18 +1,6 @@
 var db = require('../config/dbconfig.js');
 var servedata = require('../helpers/servedata');
 
-
-exports.emitUsers = function () {
-	var sql = "SELECT * FROM users";
-	db.query(sql, function (err, result, fields) {
-		if (err) {
-			io.emit('users', null);
-		} else {
-			io.emit('users', result);
-		}
-	});
-}
-
 // get users list
 exports.users_list = function (req, res) {
 
@@ -55,7 +43,7 @@ exports.users_create = function (req, res) {
 				if (err) {
 					servedata.sendError(200, err, res);
 				} else {
-					// if user found with entered email address sedn error.
+					// if user found with entered email address send error.
 					if (!result) {
 						servedata.sendErrorWithMessage(200, "Email address already registered!" + result, res);
 					} else {
@@ -70,6 +58,8 @@ exports.users_create = function (req, res) {
 									if (err) {
 										servedata.sendError(200, err, res);
 									} else {
+										//emit user
+										io.emit('new_user', result[0]);
 										servedata.sendResult(res, result);
 									}
 								})
--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,6 @@ var router = express.Router();
 //required controller modules
 var user_controller = require('../controllers/usersController');
 
-user_controller.emitUsers();
-
 /* GET users listing. */
 router.get('/', user_controller.users_list);
 
